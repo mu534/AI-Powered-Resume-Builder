e4dfd219aa809c9cebe2dd38489d8402f20d689a
mudasir najimudin <mudassirnajimuddin@gmail.com>
2025-03-13 03:33:05 +0000

    fixed backend error

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ dotenv.config();
 
 const app = express();
 app.use(express.json());
-app.use(cors({ origin: "*" }));
+app.use(cors({ origin: "https://resumebooster.vercel.app" }));
 
 // MongoDB connection
 mongoose
