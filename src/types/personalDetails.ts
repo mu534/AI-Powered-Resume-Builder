842db042931a34406c0cb499145dba2ca5419e3e
mudasir najimudin <mudassirnajimuddin@gmail.com>
2025-03-13 20:17:50 +0000

    Add logging for static file serving

--- src/types/personalDetails.ts
+++
@@ -1,9 +0,0 @@
-// types.ts (or any appropriate file in your project, e.g., src/types.ts)
-export interface PersonalDetails {
-  firstName: string;
-  lastName: string;
-  jobTitle: string;
-  address: string;
-  phone: string;
-  email: string;
-}
